974e55870f032a17f77802819d87c079f48940c0
Steven Benisek <steven.benisek@gmail.com>
2013-09-05 20:55:12 +0200

    Switch to DocBlock-esque commenting

--- jquery.plugin.js
+++ jquery.plugin.js
@@ -1,45 +1,84 @@
+/**
+ * @library <%= plugin name %>
+ * @description <%= description %>
+ * @author <%= author_name %> <<%= author_email %>>
+ * @license <%= license %>
+ */
+
 ;(function(window, document, $, undefined) {
 
     'use strict';
 
-    // Plugin namespace
-    var NAMESPACE = 'plugin';
+    /**
+     * Plugin NAMESPACE and SELECTOR
+     * @type {String}
+     * @api private
+     */
+    var NAMESPACE = '<%= plugin name %>',
+        SELECTOR = '[data-' + NAMESPACE + ']';
 
-    // Plugin constructor
+    /**
+     * Plugin constructor
+     * @param {Node} element
+     * @param {Object} [options]
+     * @api public
+     */
     function Plugin (element, options) {
         this.options = $.extend(true, $.fn[NAMESPACE].defaults, options);
         this.$element = $(element);
     }
 
-    // Plugin prototype
+    /**
+     * Plugin prototype
+     * @type {Object}
+     * @api public
+     */
     Plugin.prototype = {
-        // Restore constructor/prototype relationship
         constructor: Plugin,
+        version: '<%= version %>',
+        /**
+         * Init method
+         * @api public
+         */
         init: function () {
             // @todo add method logic
         }
         // @todo add methods
     };
 
-    // Plugin definition
+    /**
+     * jQuery plugin definition
+     * @param  {String} [method]
+     * @param  {Object} [options]
+     * @return {Object}
+     * @api public
+     */
     $.fn[NAMESPACE] = function (method, options) {
         return this.each(function () {
             var $this = $(this),
-                data = $this.data(NAMESPACE),
-                options = (typeof method == 'object') ? method : options;
-            !data && $this.data(NAMESPACE, (data = new Plugin(this, options)));
-            data[(typeof method == 'string') ? method : 'init']();
+                data = $this.data('fn.' + NAMESPACE);
+            options = (typeof method === 'object') ? method : options;
+            if (!data) {
+                $this.data('fn.' + NAMESPACE, (data = new Plugin(this, options)));
+            }
+            data[(typeof method === 'string') ? method : 'init']();
         });
     };
 
-    // Plugin defaults
+    /**
+     * jQuery plugin defaults
+     * @type {Object}
+     * @api public
+     */
     $.fn[NAMESPACE].defaults = {
         // @todo add defaults
     };
 
-    // Plugin DOM binding
-    // @todo specify target element(s)
-    $(document).on('click.' + NAMESPACE, function (event) {
+    /**
+     * jQuery plugin data api
+     * @api public
+     */
+    $(document).on('click.' + NAMESPACE, SELECTOR, function (event) {
         $(this)[NAMESPACE]();
         event.preventDefault();
     });
